8bee3d8ee444c75e0b4d95f1682f56c53f74d243
MZ <109644711+Imutigato13@users.noreply.github.com>
2023-08-24 17:58:15 -0500

    Add files via upload

---
+++ Punto 9.py
@@ -0,0 +1,21 @@
+a = 2
+b = 3
+c = 4
+d = 5
+
+if a> b and a > c and a> d:
+    x = a
+if b> a and b > c and b> d:
+    x = b
+if c> b and c > a and c> d:
+    x = c
+if d> b and d > c and d> a:
+    x = d
+if a< b and a < b and a< c:
+    y = a
+if b< a and b < c and b< d:
+    y = b
+if c< b and c < a and c< d:
+    y = c
+if d< b and d < c and d< a:
+    y = d
