11e2f11fc3e5f4eaf4b8f93bd5f4e1ce8a476a17
MZ <109644711+Imutigato13@users.noreply.github.com>
2023-08-24 17:24:35 -0500

    Delete Ejercicio 1.py

--- Ejercicio 1.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Dime un numero: "))
-b = int(input("Dime otro numero: "))
-
-if a == b:
-    print ("Los numeros son iguales")
-elif a < b:
-    print ("El segundo numero es mayor que el primero")
-elif a > b:
-    print ("El primer numero es mayor que el segundo")
