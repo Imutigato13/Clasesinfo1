bf70f5094a94ff113f2d44f208a80cbba35f35d1
MZ <109644711+Imutigato13@users.noreply.github.com>
2023-08-24 12:29:39 -0500

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -4,7 +4,7 @@ c = float(input("Ingresa la nota del tercero parcial: "))
 d = float(input("Ingresa la nota del cuarto parcial: "))
 e = float(input("Ingresa la nota del quinto parcial: "))
 
-f = (a*0.30+b*0.15+c*0.15+d*0.20+e*0.20)
+f = ((a*0.30)+(b*0.15)+(c*0.15)+(d*0.20)+(e*0.20))
 
 if f > 5:
     print("Esa nota es imposible, vuelve a digitar tus notas")
