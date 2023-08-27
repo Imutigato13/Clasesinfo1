a6e56bea49a765f722d19e1d2bcf8feca1114016
MZ <109644711+Imutigato13@users.noreply.github.com>
2023-08-27 10:34:46 -0500

    Update Ejercicio 4.py

--- Ejercicios clase 6/Ejercicio 4.py
+++ Ejercicios clase 6/Ejercicio 4.py
@@ -1,8 +1,8 @@
-n = int(input("Digite hasta que numero decea analizar"))
+n = int(input("Digite hasta que numero decea analizar: "))
 x = 0
 for n in range(1,n+1):
     if n % 2 != 0:
       x = n+x
-      print(f"X en esta iteracion es igual a {x}")
-print(f"X es igual a {x}")
+ 
+print(f"La suma de los elementos impares entre 1 y el digito elegido es igual a {x}")
    
