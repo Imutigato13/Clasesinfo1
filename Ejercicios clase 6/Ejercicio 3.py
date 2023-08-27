95d0d5096e2e1ca21b4fe3e6589f252ff1fe8da7
MZ <109644711+Imutigato13@users.noreply.github.com>
2023-08-27 10:08:18 -0500

    Update Ejercicio 3.py

--- Ejercicios clase 6/Ejercicio 3.py
+++ Ejercicios clase 6/Ejercicio 3.py
@@ -2,10 +2,10 @@ def calcular_potencia(b, e):
     return b ** e
 
 n = int(input("Ingresa el valor a elevar (0-9): "))
-x = int(input("Ingresa el valor exponete (positivo hasta 9): "))
+x = int(input("Ingresa el valor exponente (positivo hasta 9): "))
 
 if 0 <= n <= 9 and x > 0 and x <= 9:
-    for x in range(n + 1):
+    for x in range(x + 1):
         r = calcular_potencia(n, x)
         print(f"{n}^{x} = {r}")
 else:
