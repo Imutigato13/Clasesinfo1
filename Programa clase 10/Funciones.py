6c08c33336a0c1c4c8782f502c9779e9ca332d5f
MZ <109644711+Imutigato13@users.noreply.github.com>
2023-09-14 17:38:43 -0500

    Add files via upload

---
+++ Programa clase 10/Funciones.py
@@ -0,0 +1,13 @@
+def suma(x,y):
+    return(x+y)
+
+def tuplas(x,y,z,w,e)
+    nombre = x
+    
+    return()
+
+def suma(x,y):
+    return(x+y)
+
+def suma(x,y):
+    return(x+y)
---
+++ Programa clase 10/Implementacion.py
@@ -0,0 +1,19 @@
+from Funciones import *
+
+import datetime
+
+lista_global = []
+tupla_individual = ()
+
+
+print("Bienvenido")
+
+while True:
+    des = int(input("""
+                    \r1. Ingresar paciente
+                    \r2. Buscar paciente
+                    \r3. Salir
+                    \r:"""))
+    if des == 1:
+
+
